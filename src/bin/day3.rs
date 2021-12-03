ea3353fc75f80081b1b99b7f1a2ddb90fc8f44da
Tom Kendrick <tom@appointedd.com>
2021-12-03 22:05:12 +0000

    refactor a bit

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 
-fn generate_bit_occurance_map (binary_code_list: &Vec<&str>) -> BTreeMap<usize, (i32, i32)>{
+fn generate_bit_occurance_map(binary_code_list: &Vec<&str>) -> BTreeMap<usize, (i32, i32)> {
     let mut occurance_map: BTreeMap<usize, (i32, i32)> = BTreeMap::new();
     for binary_code in binary_code_list {
         for (i, bit) in binary_code.chars().enumerate() {
@@ -20,10 +20,9 @@ fn generate_bit_occurance_map (binary_code_list: &Vec<&str>) -> BTreeMap<usize, 
     occurance_map
 }
 
-fn solve_part_1(input: &Vec<&str>) -> isize {
+fn solve_part_1(occurance_map: BTreeMap<usize, (i32, i32)>) -> isize {
     let mut gamma_rate_string = String::new();
     let mut epsilon_rate_string = String::new();
-    let occurance_map = generate_bit_occurance_map(input);
     for (zero_count, one_count) in occurance_map.values() {
         if one_count > zero_count {
             gamma_rate_string.push('1');
@@ -40,9 +39,8 @@ fn solve_part_1(input: &Vec<&str>) -> isize {
 
 fn main() {
     let input = aoc2021::get_day_input(3);
-    let parsed_input = input.lines().collect();
-
-    let part1 = solve_part_1(&parsed_input);
+    let occurance_map = generate_bit_occurance_map(&input.lines().collect());
+    let part1 = solve_part_1(occurance_map);
     println!("Solution to part one: {}", part1);
     // let part2 = solve_part_2(&parsed_input);
     // println!("Solution to part two: {}", part2);
@@ -50,13 +48,12 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-
     #[test]
     fn part_one_test() {
         let test_data = vec![
             "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
             "11001", "00010", "01010",
         ];
-        assert_eq!(solve_part_1(&test_data), 198);
+        assert_eq!(solve_part_1(generate_bit_occurance_map(&test_data)), 198);
     }
 }
