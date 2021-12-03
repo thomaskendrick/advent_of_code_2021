e556404fd70617ba516c5b2e7075d414675696ec
Tom Kendrick <tom@appointedd.com>
2021-12-03 21:53:20 +0000

    refactored some shit code

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,37 +1,46 @@
 use std::collections::BTreeMap;
 
-fn solve_part_1(input: &Vec<&str>) -> isize {
-    let mut occurance_map: BTreeMap<usize, i32> = BTreeMap::new();
-    for binary_code in input {
+fn generate_bit_occurance_map (binary_code_list: &Vec<&str>) -> BTreeMap<usize, (i32, i32)>{
+    let mut occurance_map: BTreeMap<usize, (i32, i32)> = BTreeMap::new();
+    for binary_code in binary_code_list {
         for (i, bit) in binary_code.chars().enumerate() {
             match bit {
-                '0' => {*occurance_map.entry(i).or_insert(0) += -1},
-                '1' => {*occurance_map.entry(i).or_insert(0) += 1},
-                _ => panic!("Quantum computing not implemented")
+                '0' => {
+                    let (zero_count, _) = occurance_map.entry(i).or_insert((0, 0));
+                    *zero_count += 1
+                }
+                '1' => {
+                    let (_, one_count) = occurance_map.entry(i).or_insert((0, 0));
+                    *one_count += 1
+                }
+                _ => panic!("Quantum computing not implemented"),
             }
         }
     }
+    occurance_map
+}
+
+fn solve_part_1(input: &Vec<&str>) -> isize {
     let mut gamma_rate_string = String::new();
     let mut epsilon_rate_string = String::new();
-    for (_key, value) in occurance_map {
-        if value > 0 {
+    let occurance_map = generate_bit_occurance_map(input);
+    for (zero_count, one_count) in occurance_map.values() {
+        if one_count > zero_count {
             gamma_rate_string.push('1');
             epsilon_rate_string.push('0');
-        } else if value < 0 {
+        } else {
             gamma_rate_string.push('0');
             epsilon_rate_string.push('1');
-        }    
+        }
     }
-    let gamma_rate: isize = isize::from_str_radix(&gamma_rate_string, 2).unwrap() ;
-    let epsilon_rate: isize = isize::from_str_radix(&epsilon_rate_string, 2).unwrap() ;
+    let gamma_rate = isize::from_str_radix(&gamma_rate_string, 2).unwrap();
+    let epsilon_rate = isize::from_str_radix(&epsilon_rate_string, 2).unwrap();
     gamma_rate * epsilon_rate
 }
 
 fn main() {
     let input = aoc2021::get_day_input(3);
-    let parsed_input = input
-        .lines()
-        .collect();
+    let parsed_input = input.lines().collect();
 
     let part1 = solve_part_1(&parsed_input);
     println!("Solution to part one: {}", part1);
@@ -45,18 +54,8 @@ mod tests {
     #[test]
     fn part_one_test() {
         let test_data = vec![
-            "00100",
-            "11110",
-            "10110",
-            "10111",
-            "10101",
-            "01111",
-            "00111",
-            "11100",
-            "10000",
-            "11001",
-            "00010",
-            "01010"
+            "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
+            "11001", "00010", "01010",
         ];
         assert_eq!(solve_part_1(&test_data), 198);
     }
