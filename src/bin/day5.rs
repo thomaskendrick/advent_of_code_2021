553eedaa5eabc7a8dd6015319459304b3dbc6abf
Tom Kendrick <tom@appointedd.com>
2021-12-05 11:14:54 +0000

    solved part 2

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -17,24 +17,24 @@ struct LineSegment {
 impl LineSegment {
     fn walk(&self) -> Vec<Point> {
         let mut walk_points: Vec<Point> = Vec::new();
-        if self.start.x != self.end.x {
-            let range = min(self.start.x, self.end.x)..=max(self.start.x, self.end.x);
-            for i in range {
-                walk_points.push(Point {
-                    x: i,
-                    y: self.start.y,
-                })
+        let delta_x = self.end.x - self.start.x;
+        let delta_y = self.end.y - self.start.y;
+        let mut line_length_remaining = max(delta_x.abs(), delta_y.abs());
+        let mut current_pos = self.start.clone();
+
+        while line_length_remaining >= 0 {
+            walk_points.push(current_pos.clone());
+            if delta_y > 0 {
+               current_pos.y += 1 
+            } else if delta_y < 0 {
+               current_pos.y += -1 
             }
-        } else if self.start.y != self.end.y {
-            let range = min(self.start.y, self.end.y)..=max(self.start.y, self.end.y);
-            for i in range {
-                walk_points.push(Point {
-                    x: self.start.x,
-                    y: i,
-                })
+            if delta_x > 0 {
+               current_pos.x += 1 
+            } else if delta_x < 0 {
+               current_pos.x += -1 
             }
-        } else {
-            panic!("Diagonals not implemented")
+            line_length_remaining += -1;
         }
         walk_points
     }
@@ -79,12 +79,29 @@ fn solve_part_1(lines: &Vec<LineSegment>) -> i32 {
     }
     intersections
 }
+fn solve_part_2(lines: &Vec<LineSegment>) -> i32 {
+    let mut visit_map: HashMap<Point, i32> = HashMap::new();
+
+    for line in lines {
+       for point in line.walk() {
+           let val = visit_map.entry(point).or_insert(0);
+           *val += 1;
+       }
+    }
+    let mut intersections = 0;
+    for (_, val) in visit_map {
+        if val > 1 {intersections += 1}
+    }
+    intersections
+}
 
 fn main() {
     let input = aoc2021::get_day_input(5);
     let line_segments: Vec<LineSegment> = input.lines().map(parse_line).collect();
     let part_1_result = solve_part_1(&line_segments);
     println!("Solution to part 1: {}", part_1_result);
+    let part_2_result = solve_part_2(&line_segments);
+    println!("Solution to part 2: {}", part_2_result);
 }
 
 #[cfg(test)]
@@ -140,10 +157,62 @@ mod tests {
         assert_eq!(
             horizontal_line.walk(),
             vec![
-                Point { x: 1, y: 9 },
-                Point { x: 2, y: 9 },
-                Point { x: 3, y: 9 },
                 Point { x: 4, y: 9 },
+                Point { x: 3, y: 9 },
+                Point { x: 2, y: 9 },
+                Point { x: 1, y: 9 },
+            ]
+        );
+        let up_right_line = LineSegment {
+            start: Point { x: 0, y: 0 },
+            end: Point { x: 3, y: 3 },
+        };
+        assert_eq!(
+            up_right_line.walk(),
+            vec![
+                Point { x: 0, y: 0 },
+                Point { x: 1, y: 1 },
+                Point { x: 2, y: 2 },
+                Point { x: 3, y: 3 },
+            ]
+        );
+        let up_left_line = LineSegment {
+            start: Point { x: 3, y: 0 },
+            end: Point { x: 0, y: 3 },
+        };
+        assert_eq!(
+            up_left_line.walk(),
+            vec![
+                Point { x: 3, y: 0 },
+                Point { x: 2, y: 1 },
+                Point { x: 1, y: 2 },
+                Point { x: 0, y: 3 },
+            ]
+        );
+        let down_left_line = LineSegment {
+            start: Point { x: 3, y: 3 },
+            end: Point { x: 0, y: 0 },
+        };
+        assert_eq!(
+            down_left_line.walk(),
+            vec![
+                Point { x: 3, y: 3 },
+                Point { x: 2, y: 2 },
+                Point { x: 1, y: 1 },
+                Point { x: 0, y: 0 },
+            ]
+        );
+        let down_right_line = LineSegment {
+            start: Point { x: 0, y: 3 },
+            end: Point { x: 3, y: 0 },
+        };
+        assert_eq!(
+            down_right_line.walk(),
+            vec![
+                Point { x: 0, y: 3 },
+                Point { x: 1, y: 2 },
+                Point { x: 2, y: 1 },
+                Point { x: 3, y: 0 },
             ]
         );
     }
