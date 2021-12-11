0df6b72d5a958ef0dc14c79abf93c5e3cb4d6f8f
Tom Kendrick <tom@appointedd.com>
2021-12-11 19:13:57 +0000

    accept non square arrays

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -11,9 +11,10 @@ fn parser(raw_input: &str) -> Array2<u32> {
                 .collect()
         })
         .collect();
-    let n = &lines.iter().next().expect("no data").len();
-    Array::from_shape_vec((*n, *n), lines.into_iter().flatten().collect())
-        .expect("Unable to create 2d array from input")
+    let y_size = &lines.iter().next().expect("no data").len();
+    let x_size = &lines.len();
+    Array::from_shape_vec((*x_size, *y_size), lines.into_iter().flatten().collect())
+        .expect("unable to create 2d array from input")
 }
 
 fn solve_part_1(input: &Array2<u32>) {}
@@ -27,7 +28,12 @@ fn main() {
 mod tests {
     use super::*;
     #[test]
-    fn sample_data_test() {}
+    fn sample_data_test() {
+        let raw_input = aoc2021::get_day_sample_input(9);
+        let parsed_input = parser(&raw_input);
+        print!("{}", parsed_input);
+        assert!(parsed_input.is_square());
+    }
     // #[test]
     // fn sample_data_test_pt2() {
     // }
